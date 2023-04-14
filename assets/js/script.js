ec0ac22bc86b03090645004d9298699d50effdc3
Alain Tardif <tardif.alain@gmail.com>
2023-04-14 06:05:49 +0200

    Ajout des icônes sur la page d'accueil

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ import { Player } from './models/player.js';
 // Attendre que le DOM soit chargé pour lancer le jeu
 document.addEventListener('DOMContentLoaded', () => {
   // Créer deux joueurs
-  const player1 = new Player("Thomas");
-  const player2 = new Player("Samuel");
+  const player1 = new Player("1");
+  const player2 = new Player("2");
 
   // Ajouter le deuxième joueur au premier joueur
   player1.addSecondPlayer(player2);
